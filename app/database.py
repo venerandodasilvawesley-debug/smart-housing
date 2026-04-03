a13145e6aa50cb2ab27e58e08b78648b2effa890
rafaelmsozza <rmsozza@gmail.com>
2026-04-03 19:31:15 -0400

    correção de codigo

--- app/database.py
+++ app/database.py
@@ -1,11 +1,17 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
-import os
 
-load_dotenv()
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+dotenv_path = os.path.join(os.path.dirname(BASE_DIR), '.env')
+
+load_dotenv(dotenv_path=dotenv_path)
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+if not DATABASE_URL:
+    raise ValueError(f"ERRO: DATABASE_URL não encontrada! Verifique se o arquivo .env existe em: {dotenv_path}")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -19,3 +25,4 @@ def get_db():
         yield db
     finally:
         db.close()
+        
